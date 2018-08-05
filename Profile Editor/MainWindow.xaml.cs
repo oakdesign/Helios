c559e5316e12570ab2d92c7c39c2eafb55230b80
BlueFinBima <neil.g.a.larmour@gmail.com>
2018-08-05 21:12:02 +0100

    Stuck on the Release branch and can't revert. Commiting to try and move forwards.

--- Profile Editor/MainWindow.xaml.cs
+++ Profile Editor/MainWindow.xaml.cs
@@ -71,7 +71,7 @@ namespace GadrocsWorkshop.Helios.ProfileEditor
             _layoutSerializer = new XmlLayoutSerializer(this.DockManager);
             _layoutSerializer.LayoutSerializationCallback += LayoutSerializer_LayoutSerializationCallback;
 
-            _defalutLayoutFile = System.IO.Path.Combine(ConfigManager.DocumentPath, "DefaultLayout.hly");
+            _defalutLayoutFile = System.IO.Path.Combine(ConfigManager.DocumentPath, "DefaultLayout.hply");
         }
 
         void LayoutSerializer_LayoutSerializationCallback(object sender, LayoutSerializationCallbackEventArgs e)
@@ -79,11 +79,18 @@ namespace GadrocsWorkshop.Helios.ProfileEditor
             if (Profile != null && e.Model is LayoutDocument)
             {
                 HeliosObject profileObject = HeliosSerializer.ResolveReferenceName(Profile, e.Model.ContentId);
-                HeliosEditorDocument editor =  CreateDocumentEditor(profileObject);
-                profileObject.PropertyChanged += DocumentObject_PropertyChanged;
-                e.Content = CreateDocumentContent(editor);
-                e.Model.Closed += Document_Closed;
-                AddDocumentMeta(profileObject, (LayoutDocument)e.Model, editor);
+                if (profileObject != null)
+                {
+                    HeliosEditorDocument editor = CreateDocumentEditor(profileObject);
+                    profileObject.PropertyChanged += DocumentObject_PropertyChanged;
+                    e.Content = CreateDocumentContent(editor);
+                    //DocumentPane.Children.Add((LayoutDocument)e.Model);
+                    e.Model.Closed += Document_Closed;
+                    AddDocumentMeta(profileObject, (LayoutDocument)e.Model, editor);
+                } else
+                {
+                    ConfigManager.LogManager.LogDebug("Unable to resolve Layout Document " + e.Model.ContentId);
+                }
             }
         }
 
@@ -461,7 +468,7 @@ namespace GadrocsWorkshop.Helios.ProfileEditor
         {
             if (ConfigManager.UndoManager.CanUndo || (Profile != null && Profile.IsDirty))
             {
-                MessageBoxResult result = MessageBox.Show(this, "There are changes to the current profile if you continue with out saving your changes will be lost.  Would you like to save the current profile?", "Save Changes", MessageBoxButton.YesNoCancel);
+                MessageBoxResult result = MessageBox.Show(this, "There are changes to the current profile.  If you continue without saving your changes, they will be lost.  Would you like to save the current profile?", "Save Changes", MessageBoxButton.YesNoCancel);
                 if (result == MessageBoxResult.Yes)
                 {
                     return SaveProfile();
@@ -526,10 +533,16 @@ namespace GadrocsWorkshop.Helios.ProfileEditor
                 Dispatcher.Invoke(DispatcherPriority.Background, (System.Threading.SendOrPostCallback)delegate { SetValue(StatusBarMessageProperty, ""); }, "");
 
                 // TODO Restore docking panel layout
-                string layoutFileName = Path.ChangeExtension(profile.Path, "layout");
-                if (File.Exists(layoutFileName))
+                if (profile != null)
                 {
-                    Dispatcher.Invoke(DispatcherPriority.Background, (LayoutDelegate)_layoutSerializer.Deserialize, layoutFileName);
+                    string layoutFileName = Path.ChangeExtension(profile.Path, "hply");
+                    if (File.Exists(layoutFileName))
+                    {
+                        Dispatcher.Invoke(DispatcherPriority.Background, (LayoutDelegate)_layoutSerializer.Deserialize, layoutFileName);
+                    }
+                } else
+                {
+                    ConfigManager.LogManager.LogDebug("Docking Panel Layout Problem.  Profile Object Null during restore of hply for " + path);
                 }
 
                 GC.Collect();
@@ -585,7 +598,7 @@ namespace GadrocsWorkshop.Helios.ProfileEditor
                 Dispatcher.Invoke(DispatcherPriority.Background, new Action(RemoveLoadingAdorner));
                 Dispatcher.Invoke(DispatcherPriority.Background, (System.Threading.SendOrPostCallback)delegate { SetValue(StatusBarMessageProperty, ""); }, "");
 
-                string layoutFileName = Path.ChangeExtension(profile.Path, "layout");
+                string layoutFileName = Path.ChangeExtension(profile.Path, "hply");
                 if (File.Exists(layoutFileName))
                 {
                     File.Delete(layoutFileName);
